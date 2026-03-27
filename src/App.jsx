7592ee155c41b1a4041cbde6235033739ab66b43
RaiSajid50 <raisajid94@gmail.com>
2026-03-27 12:01:22 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import { SignUp } from "./pages/SignUp.jsx";
 import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
+import { Header } from "./components/Header.jsx";
 export const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
+       
       </Routes>
     </BrowserRouter>
   );
