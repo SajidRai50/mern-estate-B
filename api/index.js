e71112da00ec230e77fbed8a6a1a7fcff9379bbd
RaiSajid50 <raisajid94@gmail.com>
2026-03-27 15:18:06 +0500

    mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
  const app =express();
+ console.log("MONGO_URI:", process.env.MONGO_URI);
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("❌ Error:", err);
+  });
  app.listen(3000,()=>{
-    console.log('server is running at port 3000!!!')
+    console.log('server is running at port 3000!!!!!!!!!!!')
  })
