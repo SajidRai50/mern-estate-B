a3642d290bff7cdd974d4bdc155abbdd31c5a11c
RaiSajid50 <raisajid94@gmail.com>
2026-03-28 11:05:40 +0500

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res,next) => {
+//...........sign Up.............
+export const signup = async (req, res, next) => {
   console.log("Request Body:", req.body);
   console.log(req.body);
 
@@ -14,6 +16,52 @@ export const signup = async (req, res,next) => {
     await newUser.save();
     res.status(201).json(`User created successfully: ${email}`);
   } catch (error) {
-   next(error)
+    next(error);
+  }
+};
+
+//...........sign In.............
+
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   try {
+//     const vaildUser = await User.findOne({ email });
+//     if (!vaildUser) return
+//     next(errorHandler(404, "user not found!"));
+//     const vaildPassword = bcryptjs.compareSync(password, vaildUser.password);
+//     if (!vaildPassword) return next(errorHandler(404, "wrong credentials!!!"));
+//     const token = jwt.sign({ id: vaildUser._id }, process.env.JWT_SECRET);
+//     res
+//       .cookie("access_token", token, { httpOnly: true })
+//       .status(200)
+//       .json(vaildUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
