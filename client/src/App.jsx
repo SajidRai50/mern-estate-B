5e5e89596e3a57774c113e59b7ca861c205348c1
RaiSajid50 <raisajid94@gmail.com>
2026-03-29 15:13:31 +0500

    update header and profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,20 @@ import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Header } from "./components/Header.jsx";
+import  {PrivateRoute}  from "./components/privateRoute.jsx"
 export const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
 
+        <Route element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
