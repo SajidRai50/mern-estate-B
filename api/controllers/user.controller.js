967c35aea2a4b6a98e292dad55e84d812b709ad4
RaiSajid50 <raisajid94@gmail.com>
2026-04-03 10:09:21 +0500

    listing API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,16 +1,8 @@
-// export const test =(
-//     (req,res)=>{
-//    res.json({
-//     message :"hello from controller"
-//    })
-//  }
-// )
 
-
-//update image
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -18,32 +10,7 @@ export const test = (req, res) => {
   });
 };
 
-// export const updateUser = async (req, res, next) => {
-//   try {
-//     const updateData = {};
-//    console.log("req.body:", req.body);
-// console.log("req.params.id:", req.params.id);
-//     if (req.body.username) updateData.username = req.body.username;
-//     if (req.body.email) updateData.email = req.body.email;
-//     if (req.body.avatar) updateData.avatar = req.body.avatar;
 
-//     if (req.body.password) {
-//       updateData.password = bcryptjs.hashSync(req.body.password, 10);
-//     }
-
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       { $set: updateData },
-//       { new: true }
-//     );
-//   console.log("updatedUser:", updatedUser);
-//     const { password, ...rest } = updatedUser._doc;
-
-//     res.status(200).json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 
 export const updateUser = async(req, res ,next) =>{
@@ -86,4 +53,20 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserListings = async (req,res,next)=>{
+
+  if(req.user.id === req.params.id){
+   try {
+     const listings = await Listing.find({userRef: req.params.id});
+     res.status(200).json(listings);
+   } catch (error) {
+  next(error)
+   }
+  }
+  else{
+    return next(errorHandler(401,'You can only view your own lisrings!!'))
+  }
+
+}
 
