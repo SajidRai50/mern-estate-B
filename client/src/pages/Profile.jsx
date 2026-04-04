5b13c7fd72d4dc6533ec20a9c431c1cbfdf433fe
RaiSajid50 <raisajid94@gmail.com>
2026-04-04 09:55:23 +0500

    delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useRef, useState } from "react";
+import { useRef, useState,useEffect  } from "react";
 import { Link } from "react-router-dom";
 import {
   updateUserStart,
@@ -8,7 +8,7 @@ import {
   deleteUserSuccess,
   deleteUserFailure,
   deleteUserStart,
-  signOutUserFailure,
+
   signOutUserStart,
 } from "../redux/user/userSlice";
 
@@ -143,7 +143,23 @@ export const Profile = () => {
     } catch (error) {
       setShowListingsError(true);
     }
+
   };
+  const handleListingDelete = async (listingId) =>{
+    try {
+    const res = await fetch(`/api/listing/delete/${listingId}`,{
+      method :'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false){
+      console.log(data.message);
+      return;
+    }
+    setUserListings((prev) =>prev.filter((listing)=>listing._id !==listingId))
+    } catch (error) {
+console.log(error.message)
+    }
+   }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -263,8 +279,8 @@ export const Profile = () => {
             </Link>
 
             <div className="flex flex-col">
-              <button className="text-red-700 uppercase">Delete</button>
-              <button className="text-red-700 uppercase">edit</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
+              <button className="text-green-700 uppercase">edit</button>
             </div>
           </div>
         ))}
