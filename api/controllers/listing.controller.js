8b737c4ba308356637e08c1b0c33b33bdb450234
RaiSajid50 <raisajid94@gmail.com>
2026-04-04 10:31:15 +0500

    updating listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,22 +10,7 @@ export const createListing = async (req,res,next)=>{
     }
 };
 
-// export const deleteListing = async(req, res, next) =>{
-//  const listing = await listing.findById(req.params.id);
-//  if(!listing){
-//     return next(errorHandler(401 ,'Listing does not exit!!! '))
-//  }
-//  if(req.user.id !== listing.userRef){
-//     return next(errorHandler(401,'!! youn only delete your own listings'))
-//  }
 
-//  try {
-//  await Listing.findByIdAndDelete(req.params.id);
-//  res.status(200).json('listing has been deleted!!!')
-//  } catch (error) {
-// next(error)
-//  }
-// };
 
 
 export const deleteListing = async (req, res, next) => {
@@ -47,3 +32,25 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) =>{
+  const listing = await Listing.findById(req.params.id);
+   if (!listing) {
+      return next(errorHandler(404, "Listing does not exist!!!"));
+    }
+
+    if (req.user.id !== listing.userRef.toString()) {
+      return next(errorHandler(401, "You can only delete your own listings"));
+    }
+
+    try {
+   const update = await Listing.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new :true}
+   );
+   res.status(200).json(update);
+    } catch (error) {
+    next(error)
+    }
+}
