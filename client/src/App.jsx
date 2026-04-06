d42215b1f644993b523c3dbb21dbc13e0c1b7be1
RaiSajid50 <raisajid94@gmail.com>
2026-04-06 09:06:12 +0500

    add iamge slider/swaiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,24 +6,28 @@ import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Header } from "./components/Header.jsx";
-import {CreateListing} from "./pages/CreateListing.jsx"
-import  {PrivateRoute}  from "./components/PrivateRoute.jsx"
+import { CreateListing } from "./pages/CreateListing.jsx";
+import { PrivateRoute } from "./components/PrivateRoute.jsx";
 import { UpdateListing } from "./pages/UpdateListing.jsx";
+import { Listing } from "./pages/Listing.jsx";
 export const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/about" element={<About/>} />
-
-        <Route element={<PrivateRoute/>}>
-          <Route path="/profile" element={<Profile/>} />
-          <Route path="/create-listing" element={<CreateListing/>} />
-          <Route path="/update-listing/:listingId" element ={<UpdateListing/>}/>
-       </Route>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
